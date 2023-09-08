335f02da3508550b586552ed1e93ee09bef80b14
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-09-08 21:55:29 +0530

    Add initial ASG configuration

---
+++ assignment-2/asg.tf
@@ -0,0 +1,11 @@
+resource "aws_autoscaling_group" "autoscaler" {
+  name             = "auto scaling group"
+  max_size         = 5
+  min_size         = 1
+  desired_capacity = 2
+
+  vpc_zone_identifier = module.vpc.public_subnets
+  launch_template {
+    id = aws_launch_template.instances_launch_template.id
+  }
+}
