fc2e4aa2c15558e1e7fac86f6198ec4ca19f6c4b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-09-09 16:07:55 +0530

    Add scale out and scale in policies

--- assignment-2/asg.tf
+++ assignment-2/asg.tf
@@ -4,9 +4,28 @@ resource "aws_autoscaling_group" "autoscaler" {
   min_size         = 1
   desired_capacity = 2
 
-  vpc_zone_identifier = module.vpc.public_subnets
+  vpc_zone_identifier       = module.vpc.public_subnets
+  health_check_type         = "ELB"
+  health_check_grace_period = 60
   launch_template {
     id = aws_launch_template.instances_launch_template.id
   }
   target_group_arns = [aws_alb_target_group.target_group.arn]
+}
+
+
+resource "aws_autoscaling_policy" "scale_out_policy" {
+  name                   = "instances-scale-up-policy"
+  autoscaling_group_name = aws_autoscaling_group.autoscaler.name
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+}
+
+resource "aws_autoscaling_policy" "scale_in_policy" {
+  name                   = "instances-scale-up-policy"
+  autoscaling_group_name = aws_autoscaling_group.autoscaler.name
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
 }
